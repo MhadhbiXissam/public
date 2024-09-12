55135fe05290ecb2a9cae1cf9dccdc94ce45c706
Mhadhbi Issam <mhadhbiXissam@gmail.com>
2024-09-12 18:04:42 +0200

    Update install_nim.sh

--- install_nim.sh
+++ install_nim.sh
@@ -3,5 +3,6 @@ wget "https://nim-lang.org/download/nim-$NIM_VERSION-linux_x64.tar.xz" -O /tmp/n
 cd /tmp/ && tar -xf nim-$NIM_VERSION-linux_x64.tar.xz
 cd /tmp/nim-$NIM_VERSION && bash build.sh
 cd /tmp/nim-$NIM_VERSION && bash install.sh /opt
+cp -r /tmp/nim-$NIM_VERSION/bin /opt/nimble
 cd /tmp && rm -rf /tmp/nim-$NIM_VERSION
 cd /tmp && rm nim-$NIM_VERSION-linux_x64.tar.xz
