8e0e9c425de6d18d4b4476e4988b6c6351ed4474
Mhadhbi Issam <mhadhbiXissam@gmail.com>
2024-09-12 18:08:24 +0200

    Update install_nim.sh

--- install_nim.sh
+++ install_nim.sh
@@ -4,5 +4,7 @@ cd /tmp/ && tar -xf nim-$NIM_VERSION-linux_x64.tar.xz
 cd /tmp/nim-$NIM_VERSION && bash build.sh
 cd /tmp/nim-$NIM_VERSION && bash install.sh /opt
 cp -r /tmp/nim-$NIM_VERSION/bin /opt/nimble
+mkdir $HOME/.nimble || true 
+cp -r /tmp/nim-$NIM_VERSION/bin $HOME/.nimble
 cd /tmp && rm -rf /tmp/nim-$NIM_VERSION
 cd /tmp && rm nim-$NIM_VERSION-linux_x64.tar.xz
